28d0fda085e55d2b47272a563774f61ed5c2473a
DmitryKorobko <korobko.dmitry@gmail.com>
2018-11-13 13:26:40 +0200

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,14 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  # Use this on the cloud IDE.
+  # host = 'example.com'
+  # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 User.create!(name:  "Stein Mann", email: "korobko.dmitry@gmail.com", password: "12345678",
-             password_confirmation: "12345678", admin: true)
+             password_confirmation: "12345678", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "test-#{n+1}@gmail.com"
   password = "password"
-  User.create!(name:  name, email: email, password: password, password_confirmation: password)
+  User.create!(name:  name, email: email, password: password, password_confirmation: password, activated: true,
+               activated_at: Time.zone.now)
 end
