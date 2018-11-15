311b61ca900143e5740856095c7b0ff8f11cf54b
DmitryKorobko <korobko.dmitry@gmail.com>
2018-11-15 16:05:30 +0200

    Changed hardcode config for aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,14 +3,13 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAJGYVU5XHSHQWMVXA',
-      :aws_secret_access_key => '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',
-      :region                => 'us-east-2',
-      :host                  => 's3.amazonaws.com',
-      :endpoint              => 'apigateway.us-east-2.amazonaws.com'
+      provider:              'AWS',
+      aws_access_key_id:     'AKIAJGYVU5XHSHQWMVXA',
+      aws_secret_access_key: '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',
+      region:                'us-east-2',
+      endpoint:              'apigateway.us-east-2.amazonaws.com'
     }
-    config.fog_directory = 'steinmann'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_public = false
   end
 end
