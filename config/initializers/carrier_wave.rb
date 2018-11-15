7f348dfe361e46bcfab2e16b5fcb6852cde1b7a4
DmitryKorobko <korobko.dmitry@gmail.com>
2018-11-15 15:24:11 +0200

    Added hardcode config for aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :region                => 'us-east-2',
       :aws_access_key_id     => 'AKIAJGYVU5XHSHQWMVXA',
-      :aws_secret_access_key => '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'
+      :aws_secret_access_key => 'SwwU2wOHWXGWh1d7e0vrDNOnHG/XJRdQ/fthMSaS',
+      :bucket                => 'steinmann'
     }
     config.fog_directory     =  'images'
   end
